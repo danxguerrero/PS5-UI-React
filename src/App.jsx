ed138c880ffec226ce2311d761861275ecdc8598
danxguerrero <danxguerrero@yahoo.com>
2025-03-03 22:49:01 -0500

    Move the Navbar skeleton into its own component

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Navbar from './Navbar';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='flex p-4 border border-red-400 items-center'>
-      <div className=' w-full flex justify-between'>
-        <div className='flex '>
-          <div className='m-3 text-xl'>Games</div>
-          <div className='m-3 text-xl'>Media</div>
-        </div>
-        <div className='flex text-2xl items-center'>
-            <i className="fa-solid fa-magnifying-glass m-3"></i>
-            <i className="fa-solid fa-gear m-3"></i>
-            <i className="fa-solid fa-circle-user m-3"></i>
-            <div className='ml-8 m-3 text-3xl'>12:00</div>
-        </div>
-      </div>
-    </div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
