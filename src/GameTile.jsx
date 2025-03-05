fe14b978f74e4c2b9c0321e81ebb1401ffefb8a3
danxguerrero <danxguerrero@yahoo.com>
2025-03-04 22:58:45 -0500

    Make GameTiles smaller

--- src/GameTile.jsx
+++ src/GameTile.jsx
@@ -1,5 +1,5 @@
 const GameTile = () => {
-    return <div className="border border-blue-400 h-40 w-40 m-6 rounded-lg"></div>
+    return <div className="border border-blue-400 h-30 w-30 m-3 rounded-lg"></div>
 }
 
 export default GameTile;
