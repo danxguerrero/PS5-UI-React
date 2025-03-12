9138afa5ab9678a6d3b9474a6dba9735f31a2886
danxguerrero <danxguerrero@yahoo.com>
2025-03-11 22:46:32 -0400

    Changed color of text in NavBar to white for better visibility

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,11 @@
 const Navbar = () => {
     return <div className="flex p-4 border border-red-400 items-center">
         <div className="w-full flex justify-between">
-            <div className="flex">
+            <div className="flex text-white">
                 <div className="m-3 text-xl">Games</div>
                 <div className="m-3 text-xl">Media</div>
             </div>
-            <div className="flex text-2xl items-center">
+            <div className="flex text-2xl items-center text-white">
                 <i className="fa-solid fa-magnifying-glass m-3"></i>
                 <i className="fa-solid fa-gear m-3"></i>
                 <i className="fa-solid fa-circle-user m-3"></i>
