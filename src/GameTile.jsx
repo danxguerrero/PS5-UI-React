ba934a428140518e59b867ff8ac0c315d6205b7d
danxguerrero <danxguerrero@yahoo.com>
2025-03-11 22:32:50 -0400

    Add temporary hover styles to the Game Tiles.

--- src/GameTile.jsx
+++ src/GameTile.jsx
@@ -1,5 +1,5 @@
 const GameTile = () => {
-    return <div className="border border-blue-400 h-30 w-30 m-3 rounded-lg"></div>
+    return <div className="border border-blue-400 h-30 w-30 m-3 rounded-lg hover:bg-red-500 hover:h-40 hover:w-40"></div>
 }
 
 export default GameTile;
